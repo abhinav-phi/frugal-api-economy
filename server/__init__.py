945e1b1b4a8139930aa0688fe14f84d620688d4b
abhinav-phi <alpha9coder@gmail.com>
2026-04-08 22:06:05 +0530

    refactor: update server init

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""Frugal Api Economy environment server components."""
-
 from .frugal_api_economy_environment import FrugalApiEconomyEnvironment
 
 __all__ = ["FrugalApiEconomyEnvironment"]
